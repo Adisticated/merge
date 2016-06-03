038a9db02fbe35d834b2f700c46c3af08c849a02
aditya <aditya@aditya.corp.aptportfolio.com>
2016-06-03 15:18:03 +0530

    first commit

---
+++ CppApplication_1/main.cpp
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: aditya
+ *
+ * Created on 3 June, 2016, 3:09 PM
+ */
+
+#include <cstdlib>
+#include <iostream>
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    
+    std::cout<<"I am master"<<std::endl;
+
+    return 0;
+}
+
